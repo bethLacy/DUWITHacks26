4814988d7354d731c8581b597b87c84e76c16983
BL Allen <beth.lacy.allen@gmail.com>
2026-03-07 14:01:48 +0000

    update with new button

--- CLIENT/scripts.js
+++ CLIENT/scripts.js
@@ -1,11 +1,12 @@
 const timetableCheck = document.getElementById("timetableCheck");
 const taskCheck = document.getElementById("taskCheck");
 const timeCheck = document.getElementById("timeCheck");
+const finishCheck = document.getElementById("finishCheck");
 
 const loadSpace = document.getElementById("loadSpace");
 
 function checkAllBoxes() {
-    if (timetableCheck.checked && taskCheck.checked && timeCheck.checked) {
+    if (timetableCheck.checked && taskCheck.checked && timeCheck.checked && finishCheck.checked) {
         loadSpace.innerHTML = '<button id="generateButton">Generate timetable</button>'; 
         const generateButton = document.getElementById("generateButton");
         generateButton.addEventListener("click", generateButtonClicked);
@@ -15,12 +16,13 @@ function checkAllBoxes() {
 timetableCheck.addEventListener("change", checkAllBoxes);
 taskCheck.addEventListener("change", checkAllBoxes);
 timeCheck.addEventListener("change", checkAllBoxes);
-
+finishCheck.addEventListener("change", checkAllBoxes);
 
 
 const timetableButton = document.getElementById("timetableButton");
 const taskButton = document.getElementById("taskButton");
 const timeButton = document.getElementById("timeButton");
+const finishButton = document.getElementById("finishButton");
 
 
 function timetableButtonClicked() {
@@ -32,10 +34,14 @@ function taskButtonClicked() {
 function timeButtonClicked() {
     loadSpace.innerHTML = `<p>Ask about times willing to work here</p>`;
 }
+function finishButtonClicked() {
+    loadSpace.innerHTML = `<p>Ask about finish date here</p>`;
+}
 
 timetableButton.addEventListener("click", timetableButtonClicked);
 taskButton.addEventListener("click", taskButtonClicked);
 timeButton.addEventListener("click", timeButtonClicked);
+finishButton.addEventListener("click", finishButtonClicked);
 
 
 
