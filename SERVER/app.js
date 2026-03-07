6ad08b108e101dd88242f998dc89458511086c5a
BL Allen <beth.lacy.allen@gmail.com>
2026-03-07 16:59:43 +0000

    back end stuff

--- SERVER/app.js
+++ SERVER/app.js
@@ -1,5 +1,60 @@
-const express = require('express')
-const app = express()
+const cors = require('cors');
+
+const path = require("path");
+const fs = require("fs");
+const commitmentsDataFile = process.env.commitments_file || "STORAGE/commitments.json";
+const commitmentsFilePath = path.join(__dirname, commitmentsDataFile);
+const tasksDataFile = process.env.tasks_file || "STORAGE/tasks.json";
+const tasksFilePath = path.join(__dirname, tasksDataFile);
+const dayDataFile = process.env.tasks_file || "STORAGE/day.json";
+const dayFilePath = path.join(__dirname, dayDataFile);
+const endDataFile = process.env.tasks_file || "STORAGE/endDate.json";
+const endFilePath = path.join(__dirname, endDataFile);
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('client'));
+
+app.post("/newTask", function(req, res){
+
+    let newTask = req.body;
+
+    try {
+        const file = fs.readFileSync(tasksFilePath, "utf8");
+        const tasks = JSON.parse(file);
+        games.push(newTask);
+        fs.writeFileSync(tasksFilePath, JSON.stringify(tasks, null, 2));
+        res.status(200).header("Content-Type", "text/plain").send("New task added");
+    } 
+    catch (e) 
+    {
+        res.status(500).header("Content-Type", "text/plain").send("The server encountered a problem");
+    }
+});
+
+app.post("/newCommitment", function(req, res){
+
+    let newCommitment = req.body;
+
+    try {
+        const file = fs.readFileSync(commitmentsFilePath, "utf8");
+        const commitments = JSON.parse(file);
+        commitments.push(newCommitment);
+        fs.writeFileSync(commitmentsFilePath, JSON.stringify(commitments, null, 2));
+        res.status(200).header("Content-Type", "text/plain").send("New commitment added");
+    } 
+    catch (e) 
+    {
+        res.status(500).header("Content-Type", "text/plain").send("The server encountered a problem");
+    }
+});
+
+app.post("/")
+
+
 
 app.get('/', function(req, resp){
    resp.send('Hello world')
