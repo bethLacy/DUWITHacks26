04ad539c787f1845d9011c33a16a47ea34bbf766
BL Allen <beth.lacy.allen@gmail.com>
2026-03-07 22:59:14 +0000

    explain button does stuff

--- CLIENT/scripts.js
+++ CLIENT/scripts.js
@@ -93,3 +93,13 @@ function generateButtonClicked() {
     loadSpace.innerHTML = `<p>Generated timetable goes here</p>`;
 }
 
+
+const helpButton = document.getElementById("helpButton");
+helpButton.addEventListener("click", helpButtonClicked);
+
+function helpButtonClicked() {
+    //explain the app
+    loadSpace.innerHTML = "<p>Welcome to our project: Rein It In, the Rosie the Reindeer Planner</p>";
+    loadSpace.innerHTML += "<p>In order to use this planner, you will need to</p>"
+}
+
