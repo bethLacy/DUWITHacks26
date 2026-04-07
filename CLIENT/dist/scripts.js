c8a185aeb687b41514cdd03d78d13cc993b351d5
BL Allen <beth.lacy.allen@gmail.com>
2026-04-07 03:32:20 +0100

    get typescript working on the front end

---
+++ CLIENT/dist/scripts.js
@@ -0,0 +1,246 @@
+"use strict";
+const timetableCheck = document.getElementById("timetableCheck");
+const taskCheck = document.getElementById("taskCheck");
+const timeCheck = document.getElementById("timeCheck");
+const finishCheck = document.getElementById("finishCheck");
+const loadSpace = document.getElementById("loadSpace");
+function checkAllBoxes() {
+    if (timetableCheck.checked && taskCheck.checked && timeCheck.checked && finishCheck.checked) {
+        loadSpace.innerHTML = '<button id="generateButton">Generate timetable</button>';
+        const generateButton = document.getElementById("generateButton");
+        generateButton.addEventListener("click", generateButtonClicked);
+    }
+}
+timetableCheck.addEventListener("change", checkAllBoxes);
+taskCheck.addEventListener("change", checkAllBoxes);
+timeCheck.addEventListener("change", checkAllBoxes);
+finishCheck.addEventListener("change", checkAllBoxes);
+const timetableButton = document.getElementById("timetableButton");
+const taskButton = document.getElementById("taskButton");
+const timeButton = document.getElementById("timeButton");
+const finishButton = document.getElementById("finishButton");
+function timetableButtonClicked() {
+    loadSpace.innerHTML = `...`; // (keep your HTML exactly the same)
+    const form = document.getElementById("add_commitment_form");
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        let commitment = Object.fromEntries(formData.entries());
+        const formJSON = JSON.stringify(commitment);
+        if (await commitmentFormIsValid(formJSON)) {
+            const response = await fetch('http://127.0.0.1:8090/newCommitment', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: formJSON
+            });
+            if (response.ok) {
+                alert("Your new commitment has been added successfully");
+            }
+            else {
+                alert("There was a problem adding the new commitment");
+            }
+            form.reset();
+        }
+        else {
+            alert("This time slot is not available for a new commitment, please try again");
+        }
+    });
+}
+function taskButtonClicked() {
+    loadSpace.innerHTML = `...`;
+    const form = document.getElementById("add_task_form");
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        let task = Object.fromEntries(formData.entries());
+        const formJSON = JSON.stringify(task);
+        const response = await fetch('http://127.0.0.1:8090/newTask', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: formJSON
+        });
+        if (response.ok) {
+            alert("Your new task has been added successfully");
+        }
+        else {
+            alert("There was a problem adding the new task");
+        }
+        form.reset();
+    });
+}
+function timeButtonClicked() {
+    loadSpace.innerHTML = `...`;
+    const form = document.getElementById("work_day_form");
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        let day = Object.fromEntries(formData.entries());
+        const formJSON = JSON.stringify(day);
+        const response = await fetch('http://127.0.0.1:8090/newDayLimits', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: formJSON
+        });
+        if (response.ok) {
+            alert("Your day limit has been added successfully");
+        }
+        else {
+            alert("There was a problem adding the day limit");
+        }
+        form.reset();
+    });
+}
+function finishButtonClicked() {
+    loadSpace.innerHTML = `...`;
+    const form = document.getElementById("end_date_form");
+    form.addEventListener('submit', async function (event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        let date = Object.fromEntries(formData.entries());
+        const formJSON = JSON.stringify(date);
+        const response = await fetch('http://127.0.0.1:8090/newEndDate', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: formJSON
+        });
+        if (response.ok) {
+            alert("Your planning period has been added successfully");
+        }
+        else {
+            alert("There was a problem adding the planning period");
+        }
+        form.reset();
+    });
+}
+async function commitmentFormIsValid(formJSON) {
+    const response = await fetch('http://127.0.0.1:8090/checkTime', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: formJSON
+    });
+    return response.ok;
+}
+timetableButton.addEventListener("click", timetableButtonClicked);
+taskButton.addEventListener("click", taskButtonClicked);
+timeButton.addEventListener("click", timeButtonClicked);
+finishButton.addEventListener("click", finishButtonClicked);
+const helpButton = document.getElementById("helpButton");
+helpButton.addEventListener("click", helpButtonClicked);
+function helpButtonClicked() {
+    //explain the app
+    loadSpace.innerHTML = "<p><em>Introduction</em></p>";
+    loadSpace.innerHTML += "<p>Welcome to our project: Rein It In, the Rosie the Reindeer Planner</p>";
+    loadSpace.innerHTML += "<p>Note: Please give all time slots in intervals of 15 minutes</p>";
+    loadSpace.innerHTML += "<p>In order to use this planner, you will need to complete the 4 steps in our checklist</p>";
+    loadSpace.innerHTML += "<p><em>1. Adding commitments:</em></p>";
+    loadSpace.innerHTML += "<p>This is where you will need to upload all of your existing commitments, such as lectures, meetings and societies</p>";
+    loadSpace.innerHTML += "<p>You should add all commitments between the start and end of your day, and until the end of the planning period  (you will define these later)</p>";
+    loadSpace.innerHTML += "<p>Note: you should format the date input as DD.MM.YYYY</p>";
+    loadSpace.innerHTML += "<p>Note: you should format times as HH:MM (24 hour time)</p>";
+    loadSpace.innerHTML += "<p><em>2. Adding tasks:</em></p>";
+    loadSpace.innerHTML += "<p>This is where you should add tasks you need to complete</p>";
+    loadSpace.innerHTML += "<p>You should add all the tasks you'd like to schedule in the planning period (even non academics!)</p>";
+    loadSpace.innerHTML += "<p>Note: you should format the date input as DD.MM.YY or D.M.YY etc (there shouldn't be any leading zeroes)</p>";
+    loadSpace.innerHTML += "<p>Note: you should format times as HH:MM (24 hour time)</p>";
+    loadSpace.innerHTML += "<p>Note: you should format the duration of the tasks as a number of hours</p>";
+    loadSpace.innerHTML += "<p><em>3. Setting working times:</em></p>";
+    loadSpace.innerHTML += "<p>This is where you should tell us what time you are willing to start working each morning, and how late you are willing to finish</p>";
+    loadSpace.innerHTML += "<p>Note: you should format times as HH:MM (24 hour time)</p>";
+    loadSpace.innerHTML += "<p><em>4. Setting the planning period:</em></p>";
+    loadSpace.innerHTML += "<p>This is where you should tell us how long you want us to schedule for</p>";
+    loadSpace.innerHTML += "<p>We will schedule your tasks between the current day, and the day you choose here</p>";
+    loadSpace.innerHTML += "<p>Note: you should format the date input as DD.MM.YY or D.M.YY etc (there shouldn't be any leading zeroes)</p>";
+    loadSpace.innerHTML += "<p><em>Final notes</em></p>";
+    loadSpace.innerHTML += "<p>Thank you for using our planner</p>";
+    loadSpace.innerHTML += "<p>We hope it can help you make the greatest academic comeback of the century</p>";
+}
+const resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", reset);
+async function reset() {
+    let response = await fetch('http://127.0.0.1:8090/reset');
+    if (response.ok) {
+        alert("Information reset successfully");
+    }
+    else {
+        alert("There was an issue reseting our information");
+    }
+}
+async function generateButtonClicked() {
+    let timetable = await getTimetable();
+    let end = await getEndDate();
+    if (timetable === null || end === null) {
+        alert("Couldn't generate timetable :(");
+        return;
+    }
+    let today = new Date();
+    let endDate = parseDate(end);
+    loadSpace.innerHTML = "";
+    for (let date = new Date(today); date <= endDate; date.setDate(date.getDate() + 1)) {
+        let dateString = formatDate(date);
+        let dailyCommitments = [];
+        for (let commitment of timetable) {
+            if (commitment.date === dateString) {
+                dailyCommitments.push(commitment);
+            }
+        }
+        dailyCommitments.sort((a, b) => {
+            return toMinutes(a.startTime) - toMinutes(b.startTime);
+        });
+        let html = `<p><b>${dateString}</b></p>`;
+        html += `<table>
+            <tr>
+                <th>Time slot</th>
+                <th>Activity name</th>
+                <th>Module</th>
+                <th>Category</th>
+            </tr>`;
+        if (dailyCommitments.length === 0) {
+            html += `<tr><td colspan="4">No commitments</td></tr>`;
+        }
+        else {
+            for (let c of dailyCommitments) {
+                html += `<tr>
+                    <td>${c.startTime} - ${c.endTime}</td>
+                    <td>${c.name}</td>
+                    <td>${c.module}</td>
+                    <td>${c.category}</td>
+                </tr>`;
+            }
+        }
+        html += `</table>`;
+        loadSpace.innerHTML += html;
+    }
+}
+function toMinutes(time) {
+    const [h, m] = time.split(":").map(Number);
+    return h * 60 + m;
+}
+async function getTimetable() {
+    try {
+        let response = await fetch('http://127.0.0.1:8090/timetable');
+        let data = await response.json();
+        return data.commitments;
+    }
+    catch (e) {
+        alert("Did not have sufficient information to generate timetable");
+        return null;
+    }
+}
+async function getEndDate() {
+    try {
+        let response = await fetch('http://127.0.0.1:8090/endDate');
+        let data = await response.json();
+        return data[0].endDate;
+    }
+    catch (e) {
+        alert("No end date found");
+        return null;
+    }
+}
+function parseDate(str) {
+    const [d, m, y] = str.split(".");
+    return new Date(Number(y), Number(m) - 1, Number(d));
+}
+function formatDate(date) {
+    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear() % 100}`;
+}
